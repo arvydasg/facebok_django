e5856ecb006cb07230e2df71464e9d87eb95cce9
arvydasg <azegaspa@gmail.com>
2022-01-20 22:20:46 +0200

    feat: now able to pick entered data

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from django import forms
 from .forms import RawProductForm
 
@@ -9,13 +8,14 @@ def homepage(request):
     if request.method == "POST":
         my_form = RawProductForm(request.POST)
         if my_form.is_valid():
-            # now the data is good
-            labukas = (my_form.cleaned_data['title'])
-            print(labukas)
+            labukas = (my_form.cleaned_data['title']) # ok so it works, the data is being read from my input
+            print(my_form.cleaned_data)
         else:
             print(my_form.errors)
-            
     context = {
         "form": my_form
     }
     return render(request, 'main/home.html', context)
+
+def function(request):
+    return render(request, 'main/result.html')
