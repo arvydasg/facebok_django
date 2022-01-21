5f3c08b8b46a9f9c6f879784656c93df9bf69ad7
arvydasg <azegaspa@gmail.com>
2022-01-21 20:48:53 +0200

    feat: Added base.html to make pages more dyamic

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django import forms
 from .forms import RawProductForm
+import pyautogui
+
 
 def homepage(request):
     my_form = RawProductForm()
     if request.method == "POST":
         my_form = RawProductForm(request.POST)
         if my_form.is_valid():
-            labukas = (my_form.cleaned_data['title']) # ok so it works, the data is being read from my input
+            labukas = (my_form.cleaned_data['title'])
             print(my_form.cleaned_data)
+            print(labukas)
         else:
             print(my_form.errors)
+            
     context = {
         "form": my_form
     }
+
     return render(request, 'main/home.html', context)
 
+# random second page
 def function(request):
-    return render(request, 'main/result.html')
+    return render(request, 'main/function.html')
+
+
