adefb9e3523c46d7f3c1c8f88286ea6cdc26a6c2
arvydasg <azegaspa@gmail.com>
2022-12-02 05:49:09 +0200

    added textarea for multiple line input

--- main/forms.py
+++ main/forms.py
@@ -9,7 +9,10 @@ from django import forms
 
 class forma(forms.Form):
     Link = forms.CharField()
-    Text = forms.CharField()
+    # Text = forms.CharField()
+    Text = forms.CharField(
+        widget=forms.Textarea(attrs={"name": "body", "style": "height: 3em;"})
+    )
     Text2 = forms.CharField()
     # Number = forms.DecimalField()
     # Category= forms.CharField()
